8c7b038ce981bf87f13d57a89e5baf4fba1a550e
ravigill3936 <gill1234ravi@gmail.com>
2026-09-08 01:37:08 -0400

    small update

--- vital_gateway/cmd/server/main.go
+++ vital_gateway/cmd/server/main.go
@@ -110,21 +110,32 @@ func main() {
 		log.Fatalf("error starting server %s", err)
 	}
 }
-
 func EnableCorsWithCredentials(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
-		if origin != "" {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-		} else {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		allowedOrigins := map[string]bool{
+			"https://iloverher.com":      true,
+			"https://www.iloverher.com": true,
 		}
 
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if allowedOrigins[origin] {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
+
+		w.Header().Set(
+			"Access-Control-Allow-Methods",
+			"GET, POST, PUT, DELETE, OPTIONS",
+		)
+
+		w.Header().Set(
+			"Access-Control-Allow-Headers",
+			"Content-Type, Authorization",
+		)
 
 		w.Header().Set("Access-Control-Allow-Private-Network", "true")
+		w.Header().Set("Vary", "Origin")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
